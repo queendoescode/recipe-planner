e492b38710e27229e62a67d47c588fcdc99bd988
queendoescode <132417286+queendoescode@users.noreply.github.com>
2023-07-07 21:58:23 -0400

    added nutritional API to count calories in ingredients

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 var searchBtnEl = document.querySelector(".search-btn");
 var userInputEl = document.querySelector("#user-input");
 var resultEl = document.querySelector("#result");
@@ -22,7 +24,7 @@ function activateSearchBtn() {
 
     console.log(userInput);
     var baseUrl = 'https://api.spoonacular.com/recipes/complexSearch';
-    var finalUrl = `${baseUrl}?query=${encodeURIComponent(userInput)}&apiKey=96dedef6d4244eee817b8f5cc4b3179f`;
+    var finalUrl = `${baseUrl}?query=${encodeURIComponent(userInput)}&apiKey=8229129838cf4de5b38ad3b9facd00f8`;
     fetch(finalUrl, {
         method: 'GET',
         credentials: 'same-origin',
@@ -65,7 +67,7 @@ resultEl.addEventListener("click", function (event) {
         var inputId = element.getAttribute("data-id");
         console.log(inputId);
     }
-    fetch(`https://api.spoonacular.com/recipes/${inputId}/information?apiKey=96dedef6d4244eee817b8f5cc4b3179f`, {
+    fetch(`https://api.spoonacular.com/recipes/${inputId}/information?apiKey=8229129838cf4de5b38ad3b9facd00f8`, {
         method: 'GET',
         credentials: 'same-origin',
         redirect: 'follow',
@@ -77,13 +79,9 @@ resultEl.addEventListener("click", function (event) {
             console.log(data);
             console.log(data.extendedIngredients.length);
 
-            for (var i = 0; i < data.extendedIngredients.length; i++) {
-                var ingreLine = document.createElement('p');
-                ingreLine.textContent = data.extendedIngredients[i].original;
-                descriptionEl.appendChild(ingreLine);
-            }
+            computeCalories(data);
         });
-    fetch(`https://api.spoonacular.com/recipes/${inputId}/analyzedInstructions?apiKey=96dedef6d4244eee817b8f5cc4b3179f`, {
+    fetch(`https://api.spoonacular.com/recipes/${inputId}/analyzedInstructions?apiKey=8229129838cf4de5b38ad3b9facd00f8`, {
         method: 'GET',
         credentials: 'same-origin',
         redirect: 'follow',
@@ -106,3 +104,77 @@ resultEl.addEventListener("click", function (event) {
 
 
 searchBtnEl.addEventListener('click', activateSearchBtn);
+
+// ----- Queen Start
+
+// CalorieNinjas API
+var calorieNinjasKey = "t1JgrLUNfPPtvjjAKrN40A==YNBpxRyU6VgWySYh";
+var calorieNinjasUrl = "https://api.calorieninjas.com/v1/nutrition";
+
+var totalCalories = 0;
+
+function getNutritionPromise(item, caloriesHeading) {
+    // Some recipes use fractions e.g. "1/4", so make sure 
+    // this will be computed in calorie count.
+    var queryText = item.original;
+    var fraction = 1.0;
+    if (item.amount < 1) {
+        fraction = item.amount;
+        queryText = `1 ${item.unit} ${item.originalName}`;
+    }
+    // Some recipes use cup for solid ingredients which produces a wrong calorie figure
+    // but we can convert to ounces (oz). Also handles fractional cups (e.g. "1/4").
+    if (item.unit == "cup") {
+        fraction = 8 * item.amount;
+        queryText = `1 oz ${item.originalName}`;
+    }
+
+    queryText = queryText.replace(" ml", "ml").replace(" gr.", "g"); 
+
+    return fetch(`${calorieNinjasUrl}?query=${queryText}`, {
+        method: 'GET',
+        credentials: 'same-origin',
+        redirect: 'follow', 
+        headers: {
+            "X-Api-Key": calorieNinjasKey
+        }
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        var calories = 0;
+        if (data.items.length > 0) {
+            // Some ingredients are "to taste" or do not have a quantity; skip these in our total.
+            // (e.g. "salt and pepper to taste")
+            if (item.unit !== "servings" && item.unit !== "serving") {
+                calories = data.items[0].calories * fraction;
+                totalCalories = totalCalories + calories;
+            }
+
+            caloriesHeading.textContent = `${Math.round(totalCalories)} Calories total`;
+        }
+        console.log(`${item.original}  Calories: ${calories}  (${item.unit})`)
+        return calories;
+    });
+}
+
+function computeCalories(data) {
+    totalCalories = 0;
+
+    // Add up calories for each ingredient
+    // and display on page in a heading
+    var caloriesHeading = document.createElement('h3');
+    descriptionEl.appendChild(caloriesHeading);
+
+    for (var i = 0; i < data.extendedIngredients.length; i++) {
+        var ingreLine = document.createElement('p');
+        ingreLine.textContent = data.extendedIngredients[i].original;
+        descriptionEl.appendChild(ingreLine);
+
+        getNutritionPromise(data.extendedIngredients[i], caloriesHeading)
+            .then(calories => {
+                ;
+            });
+    }
+}
